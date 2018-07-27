8d89a294796300189dbd65205e412d37fcbda2f6
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2018-07-27 13:33:26 +0200

    Support updating flags

--- launchdarkly/resource_feature_flag.go
+++ launchdarkly/resource_feature_flag.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/mlafeldt/go-launchdarkly/client/feature_flags"
+	"github.com/mlafeldt/go-launchdarkly/models"
 )
 
 func resourceFeatureFlag() *schema.Resource {
@@ -58,7 +59,7 @@ func resourceFeatureFlagCreate(d *schema.ResourceData, metaRaw interface{}) erro
 	}
 
 	d.SetId(key)
-	return nil
+	return resourceFeatureFlagRead(d, metaRaw)
 }
 
 func resourceFeatureFlagRead(d *schema.ResourceData, metaRaw interface{}) error {
@@ -75,13 +76,45 @@ func resourceFeatureFlagRead(d *schema.ResourceData, metaRaw interface{}) error 
 		return fmt.Errorf("Failed to get flag %q in project %q: %s", key, project, err)
 	}
 
-	d.SetId(flag.Payload.Key)
+	d.Set("key", flag.Payload.Key)
+	d.Set("name", flag.Payload.Name)
+	d.Set("permanent", !flag.Payload.Temporary)
 	return nil
 }
 
 func resourceFeatureFlagUpdate(d *schema.ResourceData, metaRaw interface{}) error {
-	// TODO
-	return nil
+	meta := metaRaw.(*Meta)
+	key := d.Get("key").(string)
+	name := d.Get("name").(string)
+	project := d.Get("project").(string)
+
+	patch := feature_flags.PatchFeatureFlagBody{
+		Comment: "Terraform",
+		Patch: []*models.PatchOperation{
+			{
+				Op:    stringPtr("replace"),
+				Path:  stringPtr("/name"),
+				Value: name,
+			},
+			{
+				Op:    stringPtr("replace"),
+				Path:  stringPtr("/temporary"),
+				Value: !d.Get("permanent").(bool),
+			},
+		},
+	}
+
+	params := feature_flags.NewPatchFeatureFlagParams().
+		WithFeatureFlagKey(key).
+		WithProjectKey(project).
+		WithPatchComment(patch)
+
+	_, err := meta.LaunchDarkly.FeatureFlags.PatchFeatureFlag(params, meta.AuthInfo)
+	if err != nil {
+		return fmt.Errorf("Failed to update flag %q in project %q: %s", key, project, err)
+	}
+
+	return resourceFeatureFlagRead(d, metaRaw)
 }
 
 func resourceFeatureFlagDelete(d *schema.ResourceData, metaRaw interface{}) error {
@@ -100,3 +133,5 @@ func resourceFeatureFlagDelete(d *schema.ResourceData, metaRaw interface{}) erro
 
 	return nil
 }
+
+func stringPtr(v string) *string { return &v }
