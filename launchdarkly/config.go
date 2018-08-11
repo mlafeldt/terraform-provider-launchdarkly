d9f6818a4b7f0252131105c02ff7286dc8150129
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2018-08-11 12:02:54 +0200

    Make golint happy

--- launchdarkly/config.go
+++ launchdarkly/config.go
@@ -7,15 +7,18 @@ import (
 	"github.com/mlafeldt/go-launchdarkly/client"
 )
 
+// Config is used to configure the creation of a LaunchDarkly client.
 type Config struct {
 	Token string
 }
 
+// Meta is used by the provider to access the LaunchDarkly API.
 type Meta struct {
 	LaunchDarkly *client.Launchdarkly
 	AuthInfo     runtime.ClientAuthInfoWriter
 }
 
+// Client returns a configured LaunchDarkly client.
 func (c *Config) Client() (interface{}, error) {
 	authInfo := runtime.ClientAuthInfoWriterFunc(func(r runtime.ClientRequest, _ strfmt.Registry) error {
 		r.SetHeaderParam("User-Agent", "Terraform/"+terraform.VersionString())
